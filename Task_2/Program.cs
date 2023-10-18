9317ae7909a044e96d8e62a22b16345b4f24f67c
«Ilya» <goncharov_im@live.ru>
2023-10-19 00:53:35 +0300

    Третья задача решена.

---
+++ Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int ReadInt(string msg) // Объявление функции, с именем ReadInt и строковым параметром msg
+{
+    Console.WriteLine(msg); // Пишем в консоль параметр msg
+    string num = Console.ReadLine(); // Функция считывания введённых данных
+    int number; // Объявление переменной 
+    number = int.Parse(num); // Присвоение переменной number числового формата
+    return number; // Возвращение значения из функции
+}
+
+int day = ReadInt("Введите номер дня недели: ");
+if (day == 6 || day == 7)
+{
+    Console.WriteLine("да");
+}
+else if (day < 6 & day > 0)
+{
+    Console.WriteLine("нет");
+}
+else
+{
+    Console.WriteLine("В неделе 7 дней, введите цифру в диапазоне 1-7.");
+}
